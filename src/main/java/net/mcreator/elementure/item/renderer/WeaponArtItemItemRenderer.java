99b5e231e6ba47fa20de8be2dfc81899a9a3246e
Goopey <59980094+Goopey@users.noreply.github.com>
2023-03-08 10:01:25 -0500

    Added base animations

--- src/main/java/net/mcreator/elementure/item/renderer/WeaponArtItemItemRenderer.java
+++ src/main/java/net/mcreator/elementure/item/renderer/WeaponArtItemItemRenderer.java
@@ -1,7 +1,9 @@
 package net.mcreator.elementure.item.renderer;
 
+import software.bernie.geckolib3.util.RenderUtils;
 import software.bernie.geckolib3.renderers.geo.GeoItemRenderer;
 import software.bernie.geckolib3.geo.render.built.GeoModel;
+import software.bernie.geckolib3.geo.render.built.GeoBone;
 import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.IAnimatableModel;
 
@@ -10,12 +12,17 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.util.Mth;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.client.renderer.block.model.ItemTransforms.TransformType;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.Minecraft;
 
+import net.mcreator.elementure.utils.AnimUtils;
 import net.mcreator.elementure.item.model.WeaponArtItemItemModel;
 import net.mcreator.elementure.item.WeaponArtItemItem;
 import net.mcreator.elementure.interfaces.RendersPlayerArms;
@@ -94,6 +101,65 @@ public class WeaponArtItemItemRenderer extends GeoItemRenderer<WeaponArtItemItem
 		stack.popPose();
 		if (this.animatable != null)
 			this.animatable.getTransformType(this.transformType);
+	}
+
+	@Override
+	public void renderRecursively(GeoBone bone, PoseStack stack, VertexConsumer bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
+		Minecraft mc = Minecraft.getInstance();
+		String name = bone.getName();
+		boolean renderingArms = false;
+		if (name.equals("LeftArm") || name.equals("RightArm")) {
+			bone.setHidden(true);
+			renderingArms = true;
+		} else {
+			bone.setHidden(this.hiddenBones.contains(name));
+		}
+		if (!this.suppressedBones.contains(name)) {
+			if (this.queuedBoneSetMovements.containsKey(name)) {
+				Vector3f pos = this.queuedBoneSetMovements.get(name);
+				bone.setPositionX(pos.x());
+				bone.setPositionY(pos.y());
+				bone.setPositionZ(pos.z());
+			}
+			if (this.queuedBoneSetRotations.containsKey(name)) {
+				Vector3f rot = this.queuedBoneSetRotations.get(name);
+				bone.setRotationX(rot.x());
+				bone.setRotationY(rot.y());
+				bone.setRotationZ(rot.z());
+			}
+			if (this.queuedBoneAddRotations.containsKey(name)) {
+				Vector3f rot = this.queuedBoneAddRotations.get(name);
+				bone.setRotationX(bone.getRotationX() + rot.x());
+				bone.setRotationY(bone.getRotationY() + rot.y());
+				bone.setRotationZ(bone.getRotationZ() + rot.z());
+			}
+		}
+		if (this.transformType.firstPerson() && renderingArms) {
+			AbstractClientPlayer player = mc.player;
+			float armsAlpha = player.isInvisible() ? 0.15f : 1.0f;
+			PlayerRenderer playerRenderer = (PlayerRenderer) mc.getEntityRenderDispatcher().getRenderer(player);
+			PlayerModel<AbstractClientPlayer> model = playerRenderer.getModel();
+			stack.pushPose();
+			RenderUtils.translate(bone, stack);
+			RenderUtils.moveToPivot(bone, stack);
+			RenderUtils.rotate(bone, stack);
+			RenderUtils.scale(bone, stack);
+			RenderUtils.moveBackFromPivot(bone, stack);
+			ResourceLocation loc = player.getSkinTextureLocation();
+			VertexConsumer armBuilder = this.currentBuffer.getBuffer(RenderType.entitySolid(loc));
+			VertexConsumer sleeveBuilder = this.currentBuffer.getBuffer(RenderType.entityTranslucent(loc));
+			if (name.equals("LeftArm")) {
+				stack.translate(-1.0f * SCALE_RECIPROCAL, 2.0f * SCALE_RECIPROCAL, 0.0f);
+				AnimUtils.renderPartOverBone(model.leftArm, bone, stack, armBuilder, packedLightIn, OverlayTexture.NO_OVERLAY, armsAlpha);
+				AnimUtils.renderPartOverBone(model.leftSleeve, bone, stack, sleeveBuilder, packedLightIn, OverlayTexture.NO_OVERLAY, armsAlpha);
+			} else if (name.equals("RightArm")) {
+				stack.translate(1.0f * SCALE_RECIPROCAL, 2.0f * SCALE_RECIPROCAL, 0.0f);
+				AnimUtils.renderPartOverBone(model.rightArm, bone, stack, armBuilder, packedLightIn, OverlayTexture.NO_OVERLAY, armsAlpha);
+				AnimUtils.renderPartOverBone(model.rightSleeve, bone, stack, sleeveBuilder, packedLightIn, OverlayTexture.NO_OVERLAY, armsAlpha);
+			}
+			stack.popPose();
+		}
+		super.renderRecursively(bone, stack, this.currentBuffer.getBuffer(this.renderType), packedLightIn, packedOverlayIn, red, green, blue, alpha);
 	}
 
 	@Override
--- src/main/java/net/mcreator/elementure/procedures/WeaponArtItemSwingProcedure.java
+++ src/main/java/net/mcreator/elementure/procedures/WeaponArtItemSwingProcedure.java
@@ -7,6 +7,6 @@ import net.mcreator.elementure.item.WeaponArtItemItem;
 public class WeaponArtItemSwingProcedure {
 	public static void execute(ItemStack itemstack) {
 		if (itemstack.getItem() instanceof WeaponArtItemItem)
-			itemstack.getOrCreateTag().putString("geckoAnim", "animation.weaponartitem.side_bash");
+			itemstack.getOrCreateTag().putString("geckoAnim", "animation.weaponartitem.bash");
 	}
 }
